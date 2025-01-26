a3c08d3d8394a8ee91a49b32a93c140e45d35241
mc-yzy15 <124588885+mc-yzy15@users.noreply.github.com>
2025-01-26 08:41:30 +0800

    Delete src directory

--- src/TranslucentSM.h
+++
@@ -1,9 +0,0 @@
-class TranslucentSM {
-public:
-    void applyTransparencySettings();
-
-private:
-    DWORD GetProcessIdByName(const wchar_t* processName);
-    bool InjectDLL(DWORD processId, const wchar_t* dllPath);
-    bool SetRegistryValues();
-};
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "TranslucentSM.h"
-
-int main() {
-    TranslucentSM translucentSM;
-    translucentSM.applyTransparencySettings();
-    return 0;
-}
