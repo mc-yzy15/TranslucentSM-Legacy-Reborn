1fb816b5c46dd057de8bf0d9eae0ac108641d6e7
mc-yzy15 <124588885+mc-yzy15@users.noreply.github.com>
2025-07-28 00:27:31 +0800

    build: 更新CMake编译器路径以使用Qt目录下的工具链
    将编译器路径从Program Files/mingw64更改为Qt安装目录下的工具链，确保构建系统使用Qt配套的编译工具

--- src/Cmake.ps1
+++ src/Cmake.ps1
@@ -7,14 +7,14 @@ Set-Location -Path build
 # 运行CMake配置（针对MinGW Makefiles生成器）
 cmake -G "MinGW Makefiles" `
   -DCMAKE_BUILD_TYPE=Release `
-  -DCMAKE_C_COMPILER="C:/Program Files/mingw64/bin/gcc.exe" `
-  -DCMAKE_CXX_COMPILER="C:/Program Files/mingw64/bin/g++.exe" `
+  -DCMAKE_C_COMPILER="C:\Qt\6.9.1\mingw_64\bin/gcc.exe" `
+  -DCMAKE_CXX_COMPILER="C:\Qt\6.9.1\mingw_64\bin/g++.exe" `
   -DCMAKE_VERBOSE_MAKEFILE=ON `
-  -DCMAKE_MAKE_PROGRAM="C:/Program Files/mingw64/bin/mingw32-make.exe" `
-  -DQt6_DIR="C:/Qt/6.9.1/mingw_64/lib/cmake/Qt6" `
+  -DCMAKE_MAKE_PROGRAM="C:\Qt\6.9.1\mingw_64\bin\mingw32-make.exe" `
+  -DQt6_DIR="C:\Qt\6.9.1\mingw_64\lib\cmake\Qt6" `
   ../src
 
 # 执行构建
   # PowerShell中获取逻辑处理器数量
   $processorCount = (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
-  & "C:/Program Files/mingw64/bin/mingw32-make.exe" -j $processorCount
+  & "C:\Qt\6.9.1\mingw_64\bin\mingw32-make.exe" -j $processorCount
