6d90eb02331d9aa4168169f35edc0018162f82d4
mc-yzy15 <124588885+mc-yzy15@users.noreply.github.com>
2025-07-28 14:24:20 +0800

    Delete build directory

--- build/CMakeFiles/TranslucentSM.dir/TranslucentSM_autogen/mocs_compilation.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/TranslucentSM.dir/TranslucentSM_autogen/mocs_compilation.cpp.obj: \
- O:\Users\Yzy15\Documents\GitHub\TranslucentSM-Legacy-Reborn\build\TranslucentSM_autogen\mocs_compilation.cpp
